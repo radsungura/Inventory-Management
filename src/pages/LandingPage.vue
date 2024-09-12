4187a4d65cbe0cab9a2d021b24b35f9227fef322
Aimé RUKUNDO <82069620+radsungura@users.noreply.github.com>
2024-09-12 17:59:00 +0200

    Update LandingPage.vue

--- src/pages/LandingPage.vue
+++ src/pages/LandingPage.vue
@@ -221,6 +221,11 @@
           />
           Your browser does not support the video tag.
         </video>
+
+          <video width="320" height="240" controls>
+            <source src="/Images/demo.mp4" type="video/mp4">
+            Your browser does not support the video tag.
+          </video>
         </div>
       </div>
     </div>
